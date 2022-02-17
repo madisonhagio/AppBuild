d73e665f9c5272e0c3460b0e4cf48ac1471ca397
madisonhagio <hagiom@hawaii.edu>
2022-02-16 17:36:15 -1000

    completed second VC

--- AppBuild/ViewController.swift
+++ AppBuild/ViewController.swift
@@ -19,16 +19,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstText: UITextView!
     @IBAction func secondButton(_ sender: Any) {
         secondButton.secondLabel = "The program recognizes evolving technology drives content creation and emerging media platforms. While honoring both art and media’s foundations, UH West O‘ahu’s Creative Media program embraces digital media literacy experienced through video, animation, video games, design, social media, web and app development, virtual and augmented reality, other forms of media communication and design and digital storytelling."
+        
+        var firstLabel = Int("5")
+        
+        var secondLabel = Int("10")
+        
+        
+        
     }
     
+    @IBAction func calculateButton(_ sender: Any) {
+        var calculateButton: String
+    }
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let textLabel = UIColor.green{
+        print("the background color will be green"){
+            
+        }else{
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         self.view.backgroundColor = UIColor.systemGray
         
+        
     }
 
 
 }
+
 
